0fede2f6c8ee009d81999b846f6b890bb0f0ec21
Tanay <98188627+Tanay-Agar@users.noreply.github.com>
2024-07-20 16:40:27 -0700

    Updated BookFactory.java with better implementation

--- BookFactory.java
+++ BookFactory.java
@@ -1,26 +1,59 @@
 package biblioConnect_v3;
 
-import java.util.Map;
+import java.security.SecureRandom;
 
-public class BookFactory {
-    public static Book createBook(String bookType, String bookId, String title, String author, String isbn, Map<String, String> additionalInfo) {
-        switch (bookType.toLowerCase()) {
-            case "physical":
-                return new PhysicalBook(bookId, title, author, isbn, additionalInfo.get("location"));
-            case "ebook":
-                return new EBook(bookId, title, author, isbn, 
-                                 additionalInfo.get("format"), 
-                                 Integer.parseInt(additionalInfo.get("fileSize")), 
-                                 additionalInfo.get("downloadLink"));
-            case "audiobook":
-                return new Audiobook(bookId, title, author, isbn, 
-                                     additionalInfo.get("format"), 
-                                     Integer.parseInt(additionalInfo.get("fileSize")), 
-                                     additionalInfo.get("downloadLink"), 
-                                     additionalInfo.get("narrator"), 
-                                     Integer.parseInt(additionalInfo.get("duration")));
-            default:
-                throw new IllegalArgumentException("Unknown book type: " + bookType);
+public abstract class Book {
+    protected String bookId;
+    protected String title;
+    protected String author;
+    protected String isbn;
+    protected String bookType;
+    protected boolean isAvailable;
+
+    public Book(String title, String author, String isbn, String bookType) {
+        this.bookId = generateBookId();
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.bookType = bookType;
+        this.isAvailable = true;
+    }
+
+    public static String generateBookId() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(6);
+        String alphaNumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < 6; i++) {
+            sb.append(alphaNumeric.charAt(random.nextInt(alphaNumeric.length())));
         }
+        return sb.toString();
+    }
+
+    // Getters and setters
+    public String getBookId() { return bookId; }
+    public void setBookId(String bookId) { this.bookId = bookId; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getIsbn() { return isbn; }
+    public String getBookType() { return bookType; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+
+    public boolean isCurrentlyBorrowed() {
+        return !isAvailable;
+    }
+
+    public abstract String getDetails();
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId='" + bookId + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", bookType='" + bookType + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
     }
 }
